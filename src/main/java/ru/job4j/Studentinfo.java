4a5bd5dd5ffb280a752014f0e7e8e25fb77a401f
BasedON <anuar.begaliyev@gmail.com>
2022-09-29 19:31:37 +0600

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Anuar");
+        System.out.println("19.03.1992");
+
+    }
+}
