e195cf319df0bc9d975f5e0241b4b537c8c1d1de
BasedON <anuar.begaliyev@gmail.com>
2022-10-03 17:18:47 +0600

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -2,12 +2,10 @@ package ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
-    String idea = "I like Java";
-    System.out.println(idea);
-    idea += " But I am a Newbie ";
-    int year = Integer.parseInt("2022");
-    idea += year;
-    System.out.println(idea);
+        short s = 1500;
+        char c = (char) s;
+        double d = c;
+        System.out.println("Вывод в консоль значения после преобразования: " + d);
 
     }
 }
